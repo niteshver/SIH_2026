23cf1f29433137c76fb0b96ea41b1fc429660fbc
niteshver <niteshv1520@gmail.com>
2026-09-09 22:45:52 +0530

    update vite.confic.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:8000', '/health': 'http://localhost:8000' } },
+  server: {
+    allowedHosts: ['helpful-growth-production-bcce.up.railway.app'],
+    proxy: {
+      '/api': 'http://localhost:8000',
+      '/health': 'http://localhost:8000',
+    },
+  },
 })
